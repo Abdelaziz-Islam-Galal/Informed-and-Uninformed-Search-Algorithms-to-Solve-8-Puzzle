b1e6ef3de79555fb3f814d5c2a76b03e99d8e142
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-03-02 23:22:26 +0200

    added data structures for algorithms

---
+++ data_structures.py
@@ -0,0 +1,68 @@
+from collections import deque
+import heapq
+from board import state
+
+class queue:
+    # using deque for efficient insertion at the beginning of the data (a list will take O(n))
+    def __init__(self):
+        self.items = deque()
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.popleft()
+        raise IndexError("Dequeue from an empty queue")
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+    
+class stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        raise IndexError("Pop from an empty stack")
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+    
+class priority_queue:
+    def __init__(self):
+        self.items = []
+
+    def insert(self, item: state):
+        heapq.heappush(self.items, item)
+
+    def pop(self):
+        if not self.is_empty():
+            return heapq.heappop(self.items)
+        raise IndexError("Pop from an empty priority queue")
+    
+    def decrease_key(self, item, new_priority):
+        for index, current_item in enumerate(self.items):
+            if current_item == item:
+                if new_priority < current_item.cost:
+                    self.items[index].cost = new_priority
+                    heapq.heapify(self.items)
+                return
+        raise ValueError("Item not found in the priority queue")
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+    
