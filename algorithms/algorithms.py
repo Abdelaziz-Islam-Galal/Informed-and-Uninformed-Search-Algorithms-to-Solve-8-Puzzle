ea2304515739836a846b6557d996fa12796546b0
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-03-06 07:04:15 +0200

    faster frontier search

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -10,18 +10,24 @@ class algorithms:
         start = board_state(self._puzzle, None, False)
         explored = set()
         frontier = queue()
+        search_frontier = set() # for o(1) search instead of O(n) in the queue
+        # o(1) because the set uses hashing
+
         frontier.enqueue(start)
+        search_frontier.add(start)
     
         while not frontier.is_empty():
             state: board_state = frontier.dequeue()
             explored.add(state)
+            search_frontier.remove(state)
 
             if state.is_goal():
                 return state
 
             for neighbor in state.neighbors:
-                if neighbor not in explored and neighbor not in frontier.items:
+                if neighbor not in explored and neighbor not in search_frontier:
                     frontier.enqueue(neighbor)
+                    search_frontier.add(neighbor)
 
         return None
     
